e986e7572ea08d89fbd69fc1f724aaa26651e491
saimbintalib <49138179+saimbintalib@users.noreply.github.com>
2021-03-09 22:34:17 +0500

    Lecture 05 Layout Practice Code

---
+++ Lecture05Layout/app/src/main/java/org/saimbintalib/lecture05layout/MainActivity.java
@@ -0,0 +1,24 @@
+package org.saimbintalib.lecture05layout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    TextView textView;
+    Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button = findViewById(R.id.button);
+        textView = findViewById(R.id.textView);
+    }
+    public  void ChangeText(View view)
+    {
+        textView.setText("Text Has Been Changed!");
+    }
+}
