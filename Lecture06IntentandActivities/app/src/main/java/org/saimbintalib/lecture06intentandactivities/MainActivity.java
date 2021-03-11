a2ddea9bd2dca503d5b608063bbd0a8c55e32f7f
saimbintalib <49138179+saimbintalib@users.noreply.github.com>
2021-03-11 23:41:19 +0500

    Lecture 06 Intent and activities practice code

---
+++ Lecture06IntentandActivities/app/src/main/java/org/saimbintalib/lecture06intentandactivities/MainActivity.java
@@ -0,0 +1,29 @@
+package org.saimbintalib.lecture06intentandactivities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void Call(View view)
+    {
+        Uri uri = Uri.parse("tel:+923084306366");
+        Intent intent = new Intent(Intent.ACTION_DIAL,uri);
+        startActivity(intent);
+    }
+    public void ShowWeb(View view)
+    {
+        Uri uri = Uri.parse("http://www.pucit.edu.pk");
+        Intent intent = new Intent(Intent.ACTION_VIEW,uri);
+        startActivity(intent);
+    }
+}
