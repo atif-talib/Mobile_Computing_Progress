21274d3ffcef944bfdde6a5480e00397a42d16fc
saimbintalib <49138179+saimbintalib@users.noreply.github.com>
2021-03-30 15:04:59 +0500

    Lecture 10 database practice code

---
+++ Lecture10Database/app/src/main/java/com/mcgroupproject/lecture10database/MainActivity.java
@@ -0,0 +1,46 @@
+package com.mcgroupproject.lecture10database;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Switch;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    Button buttonAdd,buttonView;
+    EditText editName ,editAge;
+    Switch switchIsActive;
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);
+
+        buttonAdd=findViewById(R.id.buttonAdd);
+        buttonView=findViewById(R.id.buttonView);
+
+        editName=findViewById(R.id.editTextName);
+        editAge=findViewById(R.id.editTextAge);
+        switchIsActive=findViewById(R.id.switch1);
+
+        buttonAdd.setOnClickListener(new View.OnClickListener(){
+            Customer customerModel;
+            @Override
+            public void onClick(View view){
+                customerModel=new Customer(editName.getText().toString(),Integer.parseInt(editAge.getText().toString()),switchIsActive.isChecked(),1);
+                Toast.makeText(MainActivity.this,customerModel.toString(),Toast.LENGTH_LONG).show();
+                DBHelper dbHelper = new DBHelper(MainActivity.this);
+                boolean b = dbHelper.addCustomer(customerModel);
+
+            }
+        });
+        buttonView.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Toast.makeText(MainActivity.this,"view button clicked",Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+}
