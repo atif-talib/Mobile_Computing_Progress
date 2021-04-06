642b8b6c61ea3c5d4bce314ce9a69ed109b5efe5
saimbintalib <49138179+saimbintalib@users.noreply.github.com>
2021-04-06 20:17:27 +0500

    Lecture 12 Navigation drawar pracitce code

---
+++ Lecture12NavigationDrawar/app/src/main/java/com/mcgroupproject/lecture12navigationdrawar/BookActivity.java
@@ -0,0 +1,16 @@
+package com.mcgroupproject.lecture12navigationdrawar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class BookActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_book);
+        Toast.makeText(getApplicationContext(),"Book is Clicked",Toast.LENGTH_LONG).show();
+
+    }
+}
